6c06b4aafb4f757b20d116fd3675dcb48439ad0e
sehrish97 <mishi3231@gmail.com>
2022-02-05 03:37:43 -0800

    hy

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 
-const mongoURL = 'mongodb+srv://Sehrish:<SehrishBibi123>@cluster0.xtntd.mongodb.net/NotePadretryWrites=true&w=majority';
-
+const mongoURL = 'mongodb+srv://DataBase:SehrishBibi111@cluster0.ygit6.mongodb.net/NotePadretryWrites=true&w=majority';
+//
 const connetToMongo = ()=>{
     mongoose.connect(mongoURL, ()=>{
         console.log('Connected to Mongo Successfully');
